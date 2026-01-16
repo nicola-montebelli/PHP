71d80dad33105c068dd5bd05c1ad7779737c7f70
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-01-16 17:42:00 +0100

    esercizi per casa svolti

--- cicli-php/array.php
+++ cicli-php/array.php
@@ -35,17 +35,18 @@
     ?>
 
     <?php 
-        //creare un array con i primi 10 numeri interi partendo da 0
+        echo"Creare un array con i primi 10 numeri interi partendo da 0<br>";
         $numeri=[];
         for($i=0;$i<10;$i++)
         {
             $numeri[$i]=$i;
         }
-        echo"stampa dell'array con print_r <br>";
+        echo"Stampa dell'array con print_r <br>";
         print_r($numeri); //print ricorsiva mi scrive TUTTI gli elementi dell'array
         //volendo stamparli con un for
         echo "<hr>";
-        echo "stampa dell'array con un for <br>";
+
+        echo "Stampa dell'array con un for <br>";
         for($i=0;$i<count($numeri);$i++)
         {
             echo "[$i] => " . $numeri[$i] . "<br>";
@@ -55,28 +56,10 @@
 
     <?php 
         //PER CASA
-
-        // //stampare solo i valori pari di un array
-        // $array=[];
-        // for($i=0;$i<10;$i++)
-        // {
-        //     $array[$i]=$i;
-        //     if($array[$i]%2==0)              //da rivedere
-        //         {
-        //             print_r($array);
-        //         }
-        // }
-
-        
-         
-        //stampare in grassetto i numeri pari e in corsivo i dispari
-
-
-
         //creare un array di 10 numeri pari a partire da $inizio
         $inizio =rand(1,100);
         $valoriPari=[];
-        echo "10 valori pari <br>";
+        echo "10 valori pari partendo da un numero random<br>";
         while(count($valoriPari)<10)
             {
                 if($inizio%2==0)
@@ -93,21 +76,77 @@
         echo "10 valori random <br>";
         for($i=0;$i<10;$i++)
             {
-                $r[]=rand(1,10);
+                $r[]=rand(1,50);        //valori random inseriti nell'array
             }
             print_r($r);
-            echo "<hr>";
+            echo "<br>";
 
         //sommare tutti i numeri memorizzari nell'array $r
         $somma=0;
-        echo "somma dei 10 valori random dell'array \$r<br>";
+        echo "Somma dei 10 valori random dell'array \$r<br>";
         for($i=0;$i<count($r);$i++)
             {
                 $somma=$r[$i] + $somma;
             }
             echo "somma totale: " .$somma;
+            echo "<hr>";
 
-        //trovare il valore max e min dell'array $r
+            //min e max dell'array $r
+            echo "trovare il valore max e min dell'array<br>";
+            $min=+INF;
+            $max=-INF;
+            foreach($r as $controllo)
+                {
+                    if($controllo<$min)
+                        {
+                           $min = $controllo;
+                        }else if($controllo>$max)
+                        {
+                           $max=$controllo;
+                        }
+                }
+                echo "Valore minimo: " . $min ."<br>";
+                echo "Valore massimo: " .$max . "<br>";
+
+            echo "<hr>";
+    ?>
+    <?php 
+    //stampare solo i valori pari di un array
+        $array=[];
+        $solopari=[];
+        echo "Esercizio: stampare solo i valori pari di un array<br>";
+        for($i=0;$i<10;$i++)
+        {
+            $array[$i]=$i;
+            if($array[$i]%2==0){
+               $solopari[$i]=$i;
+            }
+        }
+        print_r($solopari); 
+        echo "<hr>"; 
+    ?>
+
+    <?php 
+        //stampare in grassetto i numeri pari e in corsivo i dispari
+        echo"Stampare in grassetto i numeri pari e in corsivo i dispari<br>";
+        $arraynumeri=[];
+        $lunghezzaArray= rand(5,20);
+        for($i=0;$i<$lunghezzaArray;$i++)
+            {
+                $arraynumeri[]=rand(1,30);       //creiamo i valori da mettere dentro un array di lunghezza random (5,20)
+
+                if($arraynumeri[$i]%2==0)
+                    {
+                        echo "<strong>";
+                        echo "Indice " . $i . " (" . $arraynumeri[$i] . ") " . "- ";
+                        echo "</strong>";
+                    }else
+                    {
+                        echo "<i>";
+                        echo "Indice " . $i . " [" . $arraynumeri[$i] . "] " ."- ";
+                        echo "</i>";
+                    }
+            }
     ?>
 </body>
 </html>
