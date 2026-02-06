ace7613777b1990839355cea0b23da8bc87ae983
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-02-06 12:42:35 +0100

    correzione della verifica del 06.02.26 e controllare il file su classroom dove il punto 7 è completo

---
+++ verifica_06-02-26/file_funzioni.php
@@ -0,0 +1,50 @@
+<?php
+//5 Definire una funzione che riceve in ingresso un array con i dettagli di un progetto e restituisce true se il progetto è attivo
+//nell'anno corrente,altrimenti false
+
+function progetto_valido($k,$v,$k2,$v2,$k3,$v3,$k4,$v4)
+{
+    $conto_progetti=0;
+    if($v2 == date("Y"))
+        {
+            return true;
+        }else return false;
+}
+
+//5 correzione in classe
+function attivo($progetto,$anno = null) //cambiato per la consegna 7 -  $anno = null è opzionale ma se 
+{
+    if($anno == null)
+        {
+            $anno=date("Y");
+        }
+    //anno compreso tra inizio e inizio + durata
+    //inizio <= anno <= inizio+durata($fine)
+
+    $inizio = $progetto['inizio'];                  //data dell'array in ingresso
+    //$anno = date("Y");                            //cambiato per la consegna 7
+    $fine = $inizio + $progetto['durata'];
+    if($inizio <= $anno && $anno<=$fine)
+        {
+            return true;
+        }else return false;
+}
+
+//6 correzione in classe
+function attivi($array,$anno = null)
+{
+    //prende l'array bidimensionale
+    $attivi=0;
+    foreach($array as $a)
+        {
+            if(attivo($a))      //oppure  senza funzione già definita = if($a['inizio']<=date("Y") && date("Y") <= ($a['inizio'] + $a['durata']))
+                {
+                    $attivi++;
+                }
+        }
+        return $attivi;
+}
+
+
+
+?>
---
+++ verifica_06-02-26/verifica_06-02-26.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verifica del 06.02.26 - Montebelli Nicola</title>
+</head>
+<body>
+<?php 
+include_once("file_funzioni.php");
+    $progetti=[['nome'=>'Marketing','inizio'=>'2020','durata'=>4,'partecipanti'=>30]
+               ,['nome'=>'Finanza','inizio'=>'2021','durata'=>3,'partecipanti'=>28]
+               ,['nome'=>'Automazione','inizio'=>'2023','durata'=>5,'partecipanti'=>31]
+               ,['nome'=>'AI','inizio'=>'2024','durata'=>2,'partecipanti'=>22]];
+
+    //1 Elencare i nomi di tutti i progetti
+    echo "Nomi dei progetti dell'Enaip:<br>";
+    echo "<ul>";
+    foreach($progetti as $chiave => $progetto)
+        {
+            echo "<li>".$progetto['nome'] ."</li>";
+        }
+    echo "</ul>";
+    echo "<hr>";
+
+    //2 calcolare il numero medio di durata di progetti
+    $contatore=0;
+    $somma=0;
+    foreach($progetti as $chiave => $progetto)
+        {
+            if(isset($progetto['durata']))
+                {
+                    $somma += $progetto['durata'];
+                    $contatore++;
+                }
+        }
+    echo "La media della durata dei progetti è: ". "<b>".($somma/$contatore)."</b>" ."<br>";
+    echo "<hr>";
+
+    //3 Indicare l'anno del progetto iniziato per primo. Elencare i progetti iniziati quell'anno
+    $anno = date("Y",strtotime("+1 year"));
+    $nome_progetto;
+    $progetti_paralleli=[];
+    foreach($progetti as $chiave => $progetto)
+        {
+            if($progetto['inizio']<$anno)
+                {
+                    $anno = $progetto['inizio'];
+                    $nome_progetto =$progetto['nome'];
+                    $progetti_paralleli=[];
+                }
+            if($progetto['inizio'] == $anno)
+                {
+                    $progetti_paralleli[] = $progetto['nome'];
+                }
+        }
+
+    echo "Il progetto ".$nome_progetto." è quello iniziato per prima nell'anno ".$anno."<br>";
+    //3a
+    echo "Lista delle date dei progetti che sono iniziati lo stesso anno ($anno): <br>";
+    echo "<ul>";
+    foreach($progetti_paralleli as $nome)
+        {
+            echo "<li>".$nome."</li>";
+        }
+    echo "</ul>";
+    echo "<hr>";
+
+    //4 Aggiungere il progetto PHP iniziato oggi con durata 1 anno e partecipanti in numero casuale tra 10 e 30
+    $partecipanti_rnd = rand(10,30);
+    $progetti[]= ['nome'=>'PHP','inizio'=>date("Y"),'durata'=>1,'partecipanti'=>$partecipanti_rnd];
+    print_r($progetti);
+    echo "<hr>";
+
+    //5 Definire una funzione che riceve in ingresso un array con i dettagli di un progetto e restituisce true se il progetto è attivo
+    //nell'anno corrente,altrimenti false
+    $controllo = progetto_valido('nome','nuovo2','inizio','2026','durata',3,'partecipanti',28);
+    if($controllo == true)
+        {
+            echo "Il progetto è di quest'anno";
+        }else echo "Il progetto non è di quest'anno";
+    echo "<hr>";
+
+
+    //5 correzione
+    $p= $progetti[3];   //passiamo un array singolo dell'array multidimensionale $progetti in $p
+    echo "progetto ".$p['nome']." attivo?<br>".attivo($p);      //stampa 1 se è vero non stampa nulla se false
+    echo "<hr>";
+
+
+    //6 in classe
+    echo "n. progetti attivi nell'anno corrente ". date("Y")." : ".attivi($progetti); //3 valori $prodotti[3,4,5]
+    echo "<hr>";
+
+    //7 in classe
+    echo "n. progetti attivi nell'anno scorso ". (date("Y")-1)." : ".attivi($progetti,(date("Y")-1)); //forse non va 
+
+    //controllare il file che carica la prof su classroom
+
+
+?>
+</body>
+</html>
