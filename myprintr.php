f14e29c57517e11e7951603d46caeabe1c5ad623
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-02-10 17:32:28 +0100

    myprintr svolta

---
+++ myprintr.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funzione my printr</title>
+</head>
+<body>
+    <?php 
+        function my_printr($array)
+        {
+            $i=0;
+            foreach($array as $chiave => $valore)
+                {
+                    if($chiave == $i)
+                        {
+                            echo " Array => ".$chiave;
+                            $i++;
+                        }
+                    
+                    if(is_array($valore))
+                        {
+                            
+                            my_printr($valore);
+                        }
+                    else
+                        {
+                            echo " [".$chiave."] => ";
+                            echo " (".$valore.") ";
+                        }
+                }
+        }
+    ?>
+
+
+<?php 
+        $prodotti= [ ['nome'=>'prod1', 'prezzo'=>30, 'qta'=>5, 'ultimo_acquisto'=>'2023-03-14'],
+                     ['nome'=>'prod2', 'prezzo'=>10, 'qta'=>7, 'ultimo_acquisto'=>'2024-01-12'],
+                     ['nome'=>'prod3', 'prezzo'=>20, 'qta'=>0, 'ultimo_acquisto'=>'2023-12-28'] ];
+
+        $p = ['prod1'=> 12.50,'prod2'=> 19.99,'prod3'=> 21.00,'prod4'=> 9.95];            
+
+// foreach($p as $chiave => $valore)
+//     {
+//         echo "( " .$chiave;
+//         echo " => ";            //printr fatto dall'utente
+//         echo $valore .") ";     //per un array associativo
+//     }
+// echo "<hr>";
+// foreach($prodotti as $key => $array)
+//     {
+//         echo "[Array ".$key."]"." => ";
+//         foreach($array as $chiave => $valore)
+//             {
+//                 echo "(" .$chiave;
+//                 echo " => ";            
+//                 echo $valore .") ";
+//             }
+//     }
+
+ my_printr($prodotti);
+ echo "<hr>";
+ print_r($prodotti);
+
+?>
+</body>
+</html>
