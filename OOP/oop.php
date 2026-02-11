20aa2c8526696b9140c3c5e71704d3737f8fdb57
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-02-11 11:33:23 +0100

    OOP

--- OOP/oop.php
+++ OOP/oop.php
@@ -4,6 +4,16 @@
     {
         //proprietà = variabile
         public $saluto = "Buonasera";
+        public $nome;
+        static public $testo ="proprietà statica";   //tutte le istanze usano la stessa proprietà STATICA
+
+        //PER CASA creare un contatore di oggetti istanziati,cioè una proprietà statica incrementata ad ogni chiamata del costruttore
+
+        function __construct($v) //è il metodo che viene chiamato quando viene istanziato un oggetto
+        {
+            $this->nome = $v;
+            echo "Sono nel costuttore dell'oggetto Esempio con v = $v ";    
+        }
 
         //di default la visibilità di un metodo è public
         function scriviCiao($saluto = null)   
@@ -11,7 +21,7 @@
             if(!isset($saluto))
                 {
                     
-                    $this->saluto = $this->giornoSera();
+                    $this->saluto = $this->giornoSera();    //giornoSera essendo statica si può richiamara con ::
                 }
             else
                 {
@@ -21,7 +31,7 @@
             echo "$this->saluto, sono un metodo della classe Esempio<br>";    //$saluto deve essere deciso da chi richiama il metodo
         }
 
-        private function giornoSera()
+        static private function giornoSera()
         {
             $saluto = date("H")<17?"Buongiorno":"Buonasera"; 
             return $saluto;
@@ -37,12 +47,12 @@
 </head>
 <body>
     <?php 
-        $e = new Esempio;                                             //istanza di un oggetto
+        $e = new Esempio("oggetto e");                                             //istanza di un oggetto
         $e ->saluto = "Pippo";      //referenza della proprietà della classe. Possiamo anche cambiarle il valore                                    
-        echo "valore di saluto (proprietà) dell'istanza \$e: ". $e->saluto;
+        echo "Valore di saluto (proprietà) dell'istanza \$e: ". $e->saluto;
         echo "<br>";
-        $f = new Esempio;
-        echo "valore di saluto (proprietà) dell'istanza \$f: ". $f->saluto;
+        $f = new Esempio("oggetto f");
+        echo "Valore di saluto (proprietà) dell'istanza \$f: ". $f->saluto;
         echo "<hr>";
 
         $e -> scriviCiao("AO"); //$e->saluto Pippo           //referenza (->) del metodo in una classe.  L'oggetto istanziato può solo richiamare metodi e proprietà pubbliche
@@ -50,6 +60,14 @@
 
         //echo $e non si può fare
         //var_dump($e);
+        echo "<hr>";
+
+        //referenziare un elemento statico
+        echo Esempio::$testo;
+        //o anche un oggetto qualsiasi ($e::$testo)
+        echo "<br>";
+        //se cambiassimo il valore di $testo?
+        echo $f::$testo="Nuovo testo statico";
     ?>
 </body>
 </html>
