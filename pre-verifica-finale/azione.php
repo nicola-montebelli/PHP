f07e0c993b6775f93fde536cc8c87640bfa8f08b
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-02-27 11:35:41 +0100

    spaziature

--- pre-verifica-finale/azione.php
+++ pre-verifica-finale/azione.php
@@ -21,13 +21,17 @@ include_once("config/config.php");
             {
                 //1 connessione al database
                 $conn = new PDO($dsn, $user, $pass);
+
                 //2 preparazione  dello statement (query sql)
                 $sql = "SELECT * FROM utenti WHERE ute_mail LIKE :mail";
                 $st = $conn -> prepare($sql);
+
                 //3 bind
                 $st -> bindParam(':mail', $mail);
+
                 //4 esecuzione dello statement
                 $st -> execute();
+                
                 //restituzione dei dati
                 $records = $st -> fetchAll(PDO::FETCH_ASSOC);  //fetchAll restituisce TUTTI i record del db in un array associativo
                                                                //fetch restituisce solo 1 record (guardare php.net per più informazioni)
