65b48e8bb2b5b6e30b2fc0a6dc9486549dfd65ba
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-02-27 09:36:34 +0100

    modificato il commento

--- cookie-sessioni/cookie.php
+++ cookie-sessioni/cookie.php
@@ -4,8 +4,9 @@ setcookie('secondocookie',2,time()+3600); //un'ora
 setcookie('lingua','it',time()+(3600*24));
 
 setcookie('nuovocookie','new');     //cosa succede se setto un nuovo cookie dopo che $_COOKIE ha già dei cookie settati?
-//(LATO SERVER)la prima volta che viene eseguita verrà inserita nell'elenco dei cookie ma printr($_COOKIE) ritornerà array vuoto(o i valori dei cookie già esistenti)
-//quando la richiesta torna al CLIENT $_COOKIE conterrà i valori di tutti i cookie
+//(LATO SERVER)la prima volta che viene eseguita la funzione setcookie il cookie verrà inserito nell'elenco dei cookie ma printr($_COOKIE) ritornerà array vuoto(o i valori dei cookie già esistenti)
+//perchè il server ancora non conosce il valore del cookie
+//quando il CLIENT "accetta" il cookie rimanda il suo valore al server e di conseguenza $_COOKIE conterrà i valori di tutti i cookie compreso quello nuovo
 print_r($_COOKIE);
 ?>
 
